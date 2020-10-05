d93263836fa9ef252f5df927f330a3188a6d6ee1
Rik Alexander <rik.de.kort@gmail.com>
2020-10-05 08:54:09 +0200

    Updates updates

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,39 @@
 /// For this we define a test as a routine returning true or false. True for passing.
 use std::error::Error;
 use rand_chacha::ChaCha8Rng;
+use rand::prelude::*;
+
+
+#[derive(Debug)]
+struct Frozen;
+
+impl Error for Frozen {}
+
+
+impl std::fmt::Display for Frozen {
+	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+		write!(f, "Test case is frozen!")
+	}
+}
+
+
+#[derive(Debug)]
+struct StopTest;
+
+impl Error for StopTest {}
+
+impl std::fmt::Display for StopTest {
+	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+		write!(f, "Stop test early")
+	}
+}
+
+#[derive(Debug)]
+enum MinithesisError { 
+	Frozen,
+	StopTest
+}
+
 
 fn accept<T, U: Sized>(test: U) -> Option<Box<dyn Error>> {
 	None
@@ -16,6 +49,7 @@ struct TestingState<T: ?Sized, U> {
 	test_is_trivial: bool
 }
 
+// #[derive(Clone, Copy)]
 enum Status {
 	Overrun,
 	Invalid,
@@ -24,35 +58,57 @@ enum Status {
 } 
 
 struct TestCase {
-	prefix: Vec<u8>,
+	prefix: Vec<u64>,
 	random: ChaCha8Rng,
-	max_size: u32,
-	choices: Vec<u8>,
+	max_size: usize,
+	choices: Vec<u64>,
 	status: Option<Status>,
-	depth: u32,
+	depth: u64,
 }
 
 impl TestCase {
-	fn choice(self, n: u32) -> u32 {
-		n
+	fn make_choice<F: FnMut(&mut ChaCha8Rng) -> u64>(&mut self, n: u64, mut rnd_method: F) -> Result<u64, MinithesisError> {
+		match &self.status {
+			Some(s) => Err(MinithesisError::Frozen),
+			None => { 
+				if self.choices.len() >= self.max_size { 
+					Err(self.mark_status(Status::Overrun))
+				} else if self.choices.len() < self.prefix.len() {
+					Ok(self.prefix[self.choices.len()])
+				} else {
+					Ok(rnd_method(&mut self.random))
+				}	
+			}
+		}
 	}
+
+	pub fn choice(&mut self, n: u64) -> u64 {
+		self.make_choice(n, |r: &mut ChaCha8Rng| r.gen_range(0, n)).unwrap()
+	}
+
+	fn mark_status(&mut self, status: Status) -> MinithesisError {
+		match &self.status {
+			None => {self.status = Some(status); MinithesisError::StopTest}
+			Some(s) => MinithesisError::Frozen
+		}
+    }
 }
 
 
-
-fn f(x: f64) -> f64 { x * 2.0 }
 
 
 fn main() {
-	let g = |x: f64| { x * 2.0 };
-	let ts = TestingState{
-		prefix: vec![],
-		max_examples: 10000,
-		__test_function: Box::new(g),
-		valid_test_cases: 0,  // Todo: these four are all set on init
-		calls: 0,
-		result: Some(true),
-		test_is_trivial: false
+	let mut tc = TestCase{
+		prefix: vec![], 
+		random: ChaCha8Rng::seed_from_u64(0),
+		max_size: 10000,
+		choices: vec![],
+		status: None,
+		depth: 0
 	};
+	println!("{}", tc.choice(15));
+
     println!("Hello, world!");
 }
+
+
