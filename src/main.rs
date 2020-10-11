9daa994611a4c157c62303deb1cb0c6985c01376
Rik <rik.de.kort@hey.com>
2020-10-11 20:41:49 +0200

    yay test case generation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use rand::prelude::*;
-use rand_chacha::ChaCha8Rng;
 
 #[derive(Debug)]
 enum MTErr {
@@ -15,55 +14,36 @@ enum MTStatus {
     Interesting,
 }
 
-type RandomMethod = Box<dyn Fn(&mut ChaCha8Rng) -> u64>;
+type RandomMethod = Box<dyn Fn(&mut ThreadRng) -> u64>;
 struct TestCase {
     prefix: Vec<u64>,
-    random: ChaCha8Rng,
+    random: ThreadRng,
     max_size: usize,
     choices: Vec<u64>,
     status: Option<MTStatus>,
     // print_results: bool,
     depth: u64,
-    // target_score: f64
 }
 
 impl TestCase {
-    fn new() -> TestCase {
+    fn new(prefix: Vec<u64>, random: ThreadRng, max_size: usize) -> TestCase {
         TestCase {
-            prefix: vec![],
-            random: ChaCha8Rng::seed_from_u64(0), // Todo: fix up proper randomness
-            max_size: 10000,
+            prefix: prefix,
+            random: random,
+            max_size: max_size,
             choices: vec![],
             status: None,
             depth: 0,
         }
     }
 
-    /// Return an integer in the range [0, n]
-    fn choice(&mut self, n: u64) -> Result<u64, MTErr> {
-        self.make_choice(n, Box::new(move |rng| rng.gen_range(0, n + 1)))
-    }
-
-    /// Return 1 with probability p, 0 otherwise.
-    fn weighted(&mut self, p: f64) -> Result<u64, MTErr> {
-        match self.random.gen_bool(p) {
-            true => {
-                self.choices.push(1);
-                Ok(1)
-            }
-            false => {
-                self.choices.push(0);
-                Ok(0)
-            }
-        }
-    }
-
-    /// Insert a fake choice in the choice sequence
-    fn forced_choice(&mut self, n: u64) -> Result<u64, MTErr> {
+    /// Insert a definite choice in the choice sequence
+    /// N.B. All integrity checks happen here!
+    fn det_choice(&mut self, n: u64) -> Result<u64, MTErr> {
         match self.status {
             None => {
                 if self.choices.len() >= self.max_size {
-                    self.mark_status(MTStatus::Overrun)
+                    Err(self.mark_status(MTStatus::Overrun))
                 } else {
                     self.choices.push(n);
                     Ok(n)
@@ -73,8 +53,17 @@ impl TestCase {
         }
     }
 
+    /// Return 1 with probability p, 0 otherwise.
+    fn weighted(&mut self, p: f64) -> Result<u64, MTErr> {
+        if self.random.gen_bool(p) {
+            self.det_choice(1)
+        } else {
+            self.det_choice(0)
+        }
+    }
+
     /// Mark this test case as invalid
-    fn reject(&mut self) -> Result<u64, MTErr> {
+    fn reject(&mut self) -> MTErr {
         self.mark_status(MTStatus::Invalid)
     }
 
@@ -90,36 +79,23 @@ impl TestCase {
     }
 
     // Note that mark_status never returns u64
-    fn mark_status(&mut self, status: MTStatus) -> Result<u64, MTErr> {
+    fn mark_status(&mut self, status: MTStatus) -> MTErr {
         match self.status {
+            Some(_) => MTErr::Frozen,
             None => {
                 self.status = Some(status);
-                Err(MTErr::StopTest)
+                MTErr::StopTest
             }
-            Some(_) => Err(MTErr::Frozen),
         }
     }
 
-    fn make_choice(&mut self, n: u64, rnd_method: RandomMethod) -> Result<u64, MTErr> {
-        match self.status {
-            None => {
-                if self.choices.len() >= self.max_size {
-                    self.mark_status(MTStatus::Overrun)
-                } else if self.choices.len() < self.prefix.len() {
-                    let result = self.prefix[self.choices.len()];
-                    self.choices.push(result);
-                    Ok(result)
-                } else {
-                    let result = rnd_method(&mut self.random);
-                    if result > n {
-                        self.mark_status(MTStatus::Invalid)
-                    } else {
-                        self.choices.push(result);
-                        Ok(result)
-                    }
-                }
-            }
-            Some(_) => Err(MTErr::Frozen),
+    // Return an integer in the range [0, n]
+    fn choice(&mut self, n: u64) -> Result<u64, MTErr> {
+        if self.choices.len() < self.prefix.len() {
+            self.det_choice(self.prefix[self.choices.len()])
+        } else {
+            let result = self.random.gen_range(0, n + 1);
+            self.det_choice(result)
         }
     }
 }
@@ -132,6 +108,29 @@ type Possibility<T> = Box<dyn Fn(&mut TestCase) -> Result<T, MTErr>>;
 
 use std::convert::TryInto;
 
+fn map<T: 'static, U: 'static>(initial: Possibility<T>, f: impl Fn(T)->U + 'static) -> Possibility<U> {
+    Box::new(move |tc: &mut TestCase| {
+        Ok(f(tc.any(&initial)?))
+    })
+}
+
+fn bind<T: 'static, U: 'static>(initial: Possibility<T>, f: impl Fn(T)->Possibility<U> + 'static) -> Possibility<U> {
+    Box::new(move |tc: &mut TestCase| {
+        let mapped = f(tc.any(&initial)?);
+        Ok(tc.any(&mapped)?)
+    })
+}
+
+fn satisfying<T: 'static>(initial: Possibility<T>, f: impl Fn(&T)->bool + 'static) -> Possibility<T> {
+    Box::new(move |tc: &mut TestCase| {
+        for _ in (0..3) {
+            let candidate = tc.any(&initial)?;
+            if f(&candidate) { return Ok(candidate); }
+        }
+        Err(tc.reject())
+    })
+}
+
 fn integers(m: i64, n: i64) -> Possibility<i64> {
     let t: u64 = (n - m).try_into().unwrap();
     let produce = move |tc: &mut TestCase| {
@@ -141,7 +140,7 @@ fn integers(m: i64, n: i64) -> Possibility<i64> {
     Box::new(produce)
 }
 
-fn vecs<T: 'static + std::fmt::Debug>(
+fn vecs<T: 'static>(
     elements: Possibility<T>,
     min_size: usize,
     max_size: usize,
@@ -150,9 +149,9 @@ fn vecs<T: 'static + std::fmt::Debug>(
         let mut result = vec![];
         loop {
             if result.len() < min_size {
-                tc.forced_choice(1)?;
+                tc.det_choice(1)?;
             } else if result.len() + 1 >= max_size {
-                tc.forced_choice(0)?;
+                tc.det_choice(0)?;
                 break;
             } else if tc.weighted(0.9)? == 0 {
                 break;
@@ -164,104 +163,114 @@ fn vecs<T: 'static + std::fmt::Debug>(
     Box::new(produce)
 }
 
-/// For this we define a test as a routine returning true or false.
-type PropertyTest<T> = Box<dyn Fn(&mut T) -> Result<u64, MTErr>>;
-
-enum TestResult<T> {
-    Pass,
-    Unsatisfiable,
-    Failing(T),
+fn just<T: std::clone::Clone + 'static>(value: T) -> Possibility<T> {
+    Box::new(move |_tc: &mut TestCase| { Ok(value.clone()) })
 }
 
-fn run_test<T>(
-    test: Box<dyn Fn(&mut TestCase) -> bool>,
-    max_examples: usize,
-    random: ChaCha8Rng,
-) -> TestResult<T> {
-    let mark_failures_interesting = move |tc: &mut TestCase| {
-        if !test(tc) {
-            tc.mark_status(MTStatus::Interesting)
-        } else {
-            Ok(0)
-        }
-    };
-
-    let ts = TestingState {
-        random: random,
-        test_function: Box::new(mark_failures_interesting),
-        max_examples: max_examples,
-        valid_test_cases: 0,
-        calls: 0,
-        result: None,
-        test_is_trivial: false,
-    };
-
-    TestResult::<T>::Pass
+fn nothing<T>() -> Possibility<T> {
+    Box::new(|tc: &mut TestCase| { Err(tc.reject()) })
 }
 
-fn example_test(tc: &mut TestCase) -> bool {
-    let ls: Vec<i64> = tc.any(&vecs(integers(0, 100), 5, 10)).unwrap();
-    ls.iter().sum::<i64>() < 1000
+fn mix_of<T: 'static>(first: Possibility<T>, second: Possibility<T>) -> Possibility<T> {
+    Box::new(move |tc: &mut TestCase| {
+        if tc.choice(1)? == 0 { tc.any(&first) } else { tc.any(&second) }
+    })
 }
 
-fn sort_key<T>(x: &T) -> u64 {
-    0
-}
+
+
+/// We are seeing if things are interesting (true or false)
+type InterestingTest<T> = Box<dyn Fn(&mut T) -> bool>;
 
 struct TestingState {
-    random: ChaCha8Rng,
+    random: ThreadRng,
     max_examples: usize,
-    test_function: PropertyTest<TestCase>,
+    is_interesting: InterestingTest<TestCase>,
     valid_test_cases: usize,
     calls: usize,
     result: Option<Vec<u64>>,
+    best_scoring: Option<Vec<u64>>, // Todo: check type
     test_is_trivial: bool,
 }
 
 impl TestingState {
-    fn test_function(&mut self, mut test_case: TestCase) -> Result<bool, MTErr> {
-        match (self.test_function)(&mut test_case) {
-            Ok(_) => {}
-            Err(_) => {}
-        }
-
-        if test_case.status == None {
+    fn test_function(&mut self, mut test_case: TestCase) {
+        if (self.is_interesting)(&mut test_case) {
+            test_case.status = Some(MTStatus::Interesting);
+        } else if test_case.status == None {
             test_case.status = Some(MTStatus::Valid)
         }
+
         self.calls += 1;
 
-        let status = test_case.status.unwrap();
+        match test_case.status {
+            None => panic!("Didn't expect test case status to be empty!"),
+            Some(MTStatus::Invalid) => {
+                self.test_is_trivial = test_case.choices.len() == 0;
+            }
+            Some(MTStatus::Valid) => {
+                self.test_is_trivial = test_case.choices.len() == 0;
+                self.valid_test_cases += 1;
+            }
+            Some(MTStatus::Interesting) => {
+                self.test_is_trivial = test_case.choices.len() == 0;
+                self.valid_test_cases += 1;
 
-        if (status == MTStatus::Invalid
-            || status == MTStatus::Valid
-            || status == MTStatus::Interesting)
-            && test_case.choices.len() == 0
-        {
-            self.test_is_trivial = true;
-        }
-        if (status == MTStatus::Valid || status == MTStatus::Interesting) {
-            self.valid_test_cases += 1;
-
-            // Todo: implement targeting
-        }
-        if status == MTStatus::Interesting {
-            if self.result == None
-                || sort_key(self.result.as_ref().unwrap()) > sort_key(&test_case.choices)
-            {
-                self.result = Some(test_case.choices)
+                if self.result == None || self.result.as_ref().unwrap() > &test_case.choices {
+                    self.result = Some(test_case.choices)
+                }
+            }
+            Some(MTStatus::Overrun) => {
+                panic!("Test case length overrun!");
             }
         }
-
-        Ok(true)
     }
 
-    // NEXT TODO: implement run() to finish run_test for example test!
+    fn run(&mut self) {
+        self.generate();
+        self.shrink();
+    }
+
+    fn should_keep_generating(&self) -> bool {
+        (!self.test_is_trivial)
+            & (self.result == None)
+            & (self.valid_test_cases < self.max_examples)
+            & (self.calls < self.max_examples * 10)
+    }
+
+    fn generate(&mut self) {
+        while self.should_keep_generating()
+            & ((self.best_scoring == None) || self.valid_test_cases <= self.max_examples / 2)
+        {
+            self.test_function(TestCase::new(vec![], self.random.clone(), 8 * 1024));
+        }
+    }
+
+    fn shrink(&mut self) {
+        if self.result == None {
+            return;
+        }
+    }
+}
+
+/// Note that we invert the relationship: true means "interesting"
+fn example_test(tc: &mut TestCase) -> bool {
+    let ls: Vec<i64> = tc.any(&vecs(integers(95, 105), 9, 11)).unwrap();
+    println!("running with list {:?}", ls);
+    ls.iter().sum::<i64>() > 1000
 }
 
 fn main() {
-    // run_test(Box::new(example), 10, ChaCha8Rng::seed_from_u64(0));
-    let mut tc = TestCase::new();
-    let p = vecs(integers(0, 10), 10, 10);
-    println!("Selected integer: {:?}", tc.any(&p));
-    let r: u64 = (vec![1, 2, 3]).iter().sum();
+    let mut ts = TestingState {
+        random: thread_rng(),
+        is_interesting: Box::new(example_test),
+        max_examples: 10,
+        valid_test_cases: 0,
+        calls: 0,
+        result: None,
+        best_scoring: None,
+        test_is_trivial: false,
+    };
+    ts.run();
+    println!("Test result {:?}", ts.result);
 }
